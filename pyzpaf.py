2fd12de7bffd1a00448ceb6d9df4d93d9e843b35
Anas Barakat <anas.barakat@telecom-paristech.fr>
2016-06-17 18:27:31 +0200

    implementation algo6-DCT

--- pyzpaf.py
+++ pyzpaf.py
@@ -5,13 +5,15 @@ from math import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-fichier = open("C:/Users/Azoulay/Downloads/paf.txt", "r")
-epsilon = fichier.read()[3:]
+"""
+fichier = open("/Users/anasbarakat/Documents/PAF-incertitudeRepo/data.txt", "r")
+#epsilon = fichier.read()[3:]
+
 fichier.close()
 
 n = int(input("Entrée la longueur n : "))
 
-"""    Algorithme 1  """
+Algorithme 1  
 def algo1(n):
     S_n = 0
     for i in range(n):
@@ -20,7 +22,7 @@ def algo1(n):
     P_value = erfc(s_obs/sqrt(2))
     return P_value
 
-""" Algorithme 2"""
+ Algorithme 2
 def algo2(n, M):
     N = floor(n/M)
     pi = []
@@ -28,9 +30,8 @@ def algo2(n, M):
         for j in range(3):
             pi += [ 
 
-
 ## Histogramme des P_values
-f = [algo1(i) for i in range(1000,10000)]##1004882
+f= [algo1(i) for i in range(1000,10000)]
 def hist(f):
     frequence, lim, patches = plt.hist(f, range = (0, 1), bins = 10)
     plt.xlabel('Valeurs de P_value')
@@ -44,4 +45,90 @@ def circ(f):
     plt.pie(f, labels=name, autopct='%1.1f%%', startangle=90)
     plt.axis('equal')
     plt.show()
-circ(f)
+circ(f)"""
+
+
+""" Algorithme 3: Discrete Fourier Transform (Spectra) Test """
+
+epsilon1= [1,0,0,1,0,1,0,0,1,1]
+epsilon2= [1,1,0,0,1,0,0,1,0,0,0,0,1,1,1,1,1,1,0,1,1,0,1,0,1,0,1,0,0,0,1,0,0,0,1,0,0,0,0,
+           1,0,1,1,0,1,0,0,0,1,1,0,0,0,0,1,0,0,0,1,1,0,1,0,0,1,1,0,0,0,1,0,0,1,1,0,0,0,1,1,0,0,1,1,0,0,0,1,0,1,0,0,0,1,0,1,1,1,0,0,0]
+
+def epsilonToX(n, epsilon):
+    X = []
+    for i in range(n):
+        X += [2*int(epsilon[i])-1]
+    print(X)
+    return X
+        
+def DFT(n,X):
+    
+    S= np.fft.fft(X)
+    Sbis= S[:int(n/2)]
+    print("Sbis=", Sbis)
+    Mod=[]
+    
+    for k in range(len(Sbis)):
+        Mod += [abs(Sbis[k])]
+    return Mod
+        
+def P_value(n, epsilon):
+    T= sqrt(log(1/0.05)*n)
+    print("T=", T)
+    N0=0.95*n/2
+    print("N0=", N0)
+    N1=0    
+    M= DFT(n,epsilonToX(n, epsilon))
+    print("M=", M)
+    
+    for k in range(len(M)):
+        if (M[k]<T):
+            N1 +=1
+    N1= N1-1
+    print("N1=", N1 )
+    
+    d= (N1-N0)/(sqrt(n*(0.95)*(0.05)/4))
+    print("d=", d)
+    P_value= erfc(abs(d)/sqrt(2))
+    print("P_value=", P_value)
+
+    return P_value
+    
+def testDCTalgo(n, epsilon):
+    Pvalue= P_value(n, epsilon)
+    if (Pvalue < 0.01):
+        return "The sequence is NON-RANDOM"
+    else: 
+        return "The sequence is RANDOM"
+
+print(testDCTalgo(10,epsilon))
+
+
+    
+    
+
+
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
