3f7fe57375dd0544860835e034e2825de3e27695
Nathan AZOULAY <nathan.azoulay@telecom-paristech.fr>
2016-06-17 11:24:17 +0200

    debut algo2

--- pyzpaf.py
+++ pyzpaf.py
@@ -20,19 +20,28 @@ def algo1(n):
     P_value = erfc(s_obs/sqrt(2))
     return P_value
 
-print(algo1(n))
+""" Algorithme 2"""
+def algo2(n, M):
+    N = floor(n/M)
+    pi = []
+    for i in range(N):
+        for j in range(3):
+            pi += [ 
 
 
 ## Histogramme des P_values
 f = [algo1(i) for i in range(1000,10000)]##1004882
-frequence, lim, patches = plt.hist(f, range = (0, 1), bins = 10)
-plt.xlabel('Valeurs de P_value')
-plt.ylabel('Fréquence comptée')
-plt.title('Histogramme des P_values')
-##_plt.show()
+def hist(f):
+    frequence, lim, patches = plt.hist(f, range = (0, 1), bins = 10)
+    plt.xlabel('Valeurs de P_value')
+    plt.ylabel('Fréquence comptée')
+    plt.title('Histogramme des P_values')
+    plt.show()
 
 ## Diagramme circulaire
-name = ['[0;0.1]', '[0.1;0.2]', '[0.2;0.3]', '[0.3;0.4]', '[0.4;0.5]', '[0.5;0.6]', '[0.6;0.7]', '[0.8;0.9]', '[0.9;1]']
-plt.pie(f, labels=name, autopct='%1.1f%%', startangle=90)
-plt.axis('equal')
-plt.show()
+def circ(f):
+    name = ['[0;0.1]', '[0.1;0.2]', '[0.2;0.3]', '[0.3;0.4]', '[0.4;0.5]', '[0.5;0.6]', '[0.6;0.7]', '[0.8;0.9]', '[0.9;1]']
+    plt.pie(f, labels=name, autopct='%1.1f%%', startangle=90)
+    plt.axis('equal')
+    plt.show()
+circ(f)
