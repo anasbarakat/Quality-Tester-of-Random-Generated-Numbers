23b8c90d897ddb63f7993283bc479c90c8e13994
Nathan AZOULAY <nathan.azoulay@telecom-paristech.fr>
2016-06-21 10:22:18 +0200

    Sync du bouton lancer

--- gui_test.py
+++ gui_test.py
@@ -16,47 +16,62 @@ from pyzpaf import *
 
 fenetre = Tk()
 fenetre.title('Sure or not Sure That is the Question?')
-fenetre.geometry()
+
+#Ajout d'un Background
+photo = PhotoImage(file="C:/Users/Azoulay/Desktop/PAF-incertitude/de.gif")
+canvas = Canvas(fenetre,width=1, height=1)
+canvas.pack(fill=BOTH, expand=1)
+canvas.create_image(0, 0,  image=photo, anchor=NW)
+fenetre.geometry('381x307')
 
 
-""" Lignes de saisies """
+""" Lignes de saisies  """
 
 Label1 = Label(fenetre, text = 'Input Data File :')
-Label1.pack()
+Label1.pack(side = LEFT, padx = 10, pady = 10)
 File= StringVar()
 Champ1 = Entry(fenetre, textvariable= File, bg ='bisque', fg='maroon')
 Champ1.focus_set()
-Champ1.pack()
+Champ1.pack(side = LEFT, padx = 10, pady = 10)
 
 
 Label2 = Label(fenetre, text = 'BitStream length :')
-Label2.pack()
+Label2.pack( side = LEFT,padx = 10, pady = 10)
 bsl= StringVar()
 Champ2 = Entry(fenetre, textvariable= bsl, bg ='bisque', fg='maroon')
 Champ2.focus_set()
-Champ2.pack()
+Champ2.pack( side = LEFT,padx = 10, pady = 10)
 
 
 Label3 = Label(fenetre, text = 'Number of Bitestream :')
-Label3.pack()
+Label3.pack(side = LEFT, padx = 10, pady = 10)
 nob= StringVar()
 Champ3 = Entry(fenetre, textvariable= nob, bg ='bisque', fg='maroon')
 Champ3.focus_set()
-Champ3.pack()
+Champ3.pack(side = LEFT, padx = 10, pady = 10)
 
 
 Label4 = Label(fenetre, text = 'Number of Block :')
-Label4.pack()
+Label4.pack(side = LEFT, padx = 10, pady = 10)
 nobl= StringVar()
 Champ4 = Entry(fenetre, textvariable= nobl, bg ='bisque', fg='maroon')
 Champ4.focus_set()
-Champ4.pack()
+Champ4.pack(side = LEFT, padx = 10, pady = 10)
 
-photo = PhotoImage(file="de.png")
 
-canvas = Canvas(fenetre,width=350, height=200)
-canvas.create_image(0, 0, anchor=NW, image=photo)
-canvas.pack()
+Label5 = Label(fenetre)
+Label5.pack(side = BOTTOM, padx = 10, pady = 10)
+
+# Radiobutton pour le choix de L'algorithme
+val = StringVar() 
+bout1 = Radiobutton(fenetre, text="Algorithme 1", variable=val, val=1)
+bout2 = Radiobutton(fenetre, text="Algorithme 2", variable=val, val=2)
+bout3 = Radiobutton(fenetre, text="Algorithme 6", variable=val, val=3)
+bout4 = Radiobutton(fenetre, text="Algorithme 10", variable=val, val=4)
+bout1.pack()
+bout2.pack()
+bout3.pack()
+bout4.pack()
 
 # Radiobutton pour le choix du graphe
 value = StringVar() 
@@ -67,13 +82,21 @@ bouton1.pack()
 bouton2.pack()
 bouton3.pack()
 
-"""
 
 #Bouton pour lancer le Programme
-bouton_lancer = Button(text='Lancer', command=Executer)
-bouton_lancer.pack()
+def Lancer():
+    type_graphe = int(value.get())
+    algo = int(val.get())
+    file = str(Champ1.get())
+    BitStream_Length = int(Champ2.get())
+    
+    
+    Label5.config(text = 'Result : ' + pourcentage + ' %')    
+
+bouton_lancer = Button(fenetre, text='Lancer', command=Lancer)
+bouton_lancer.pack(side = BOTTOM, padx = 5, pady = 5)
  
- 
+"""
 def Executer():
     a = float(e1.get())
     b = float(e2.get())
