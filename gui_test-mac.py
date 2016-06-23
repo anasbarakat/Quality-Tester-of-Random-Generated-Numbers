86bbd6b989b1a38e142fcd0ead6a62801c1e8d79
Nathan AZOULAY <nathan.azoulay@telecom-paristech.fr>
2016-06-23 11:21:35 +0200

    ajout des boutons pour les nouveaux algo et ajout du choix du template

--- gui_test-mac.py
+++ gui_test-mac.py
@@ -88,6 +88,16 @@ Champ4.pack()
 #Champ4.place(x=175, y=100)
 Champ4.place(x=200, y=y4)
 
+y5=y4+30
+Label6 = Label(fenetre, text = 'Template Choice (Algo 7 et 8) :')
+Label6.pack()
+Label6.place(x=75, y=y5)
+tc= StringVar()
+Champ6 = Entry(fenetre, textvariable= tc, bg ='bisque', fg='maroon')
+Champ6.focus_set()
+Champ6.pack()
+#Champ4.place(x=175, y=100)
+Champ6.place(x=200, y=y5)
 
 Label5 = Label(fenetre)
 Label5.pack(side = LEFT, padx = 10, pady = 10)
@@ -99,11 +109,15 @@ y11=290
 y12=y11+30
 y13=y12+30
 y14=y13+30
+y15=y14+30
+y16=y15+30
 val = StringVar() 
-bout1 = Radiobutton(fenetre, text="Frequency Test", variable=val, val=1)
-bout2 = Radiobutton(fenetre, text="Frequency Test Within a Block", variable=val, val=2)
-bout3 = Radiobutton(fenetre, text="Discrete Fourier Transform Test", variable=val, val=3)
-bout4 = Radiobutton(fenetre, text="Linear Complexity Test", variable=val, val=4)
+bout1 = Radiobutton(fenetre, text="Frequency Test (1)", variable=val, val=1)
+bout2 = Radiobutton(fenetre, text="Frequency Test Within a Block (2)", variable=val, val=2)
+bout3 = Radiobutton(fenetre, text="Discrete Fourier Transform Test (6)", variable=val, val=3)
+bout4 = Radiobutton(fenetre, text="Linear Complexity Test (10)", variable=val, val=4)
+bout5 = Radiobutton(fenetre, text="Non-overlapping Template Matching Test (7)", variable=val, val=5)
+bout6 = Radiobutton(fenetre, text="Overlapping Template Matching Test (8)", variable=val, val=6)
 bout1.pack()
 bout1.place(x=75,y=y11)
 bout2.pack()
@@ -112,6 +126,10 @@ bout3.pack()
 bout3.place(x=75,y=y13)
 bout4.pack()
 bout4.place(x=75,y=y14)
+bout5.pack()
+bout5.place(x=75,y=y15)
+bout6.pack()
+bout6.place(x=75,y=y16)
 
 #Choix de la représentation graphique
 y21=290
@@ -191,10 +209,13 @@ def Lancer():
     type_graphe = int(value.get())
     algo = int(val.get())
     file = str(Champ1.get())
+    # fichier = open("C:/Users/Azoulay/Desktop/PAF-incertitude/"+file+".txt", "r")
+    #epsilon = fichier.read()[3:]
+    #fichier.close()
     BitStream_Length = int(Champ2.get())
     Nb_of_BitStream = int(Champ3.get())
     BlockLength = int(Champ4.get())
-
+    Template = str(Champ6.get())
     
     if(BitStream_Length< 100 and algo==1):
         messagebox.showinfo("Input Size Recommendation","Choose a minimum of 100 bits")
@@ -215,7 +236,13 @@ def Lancer():
     
     if((BlockLength < 500 or BlockLength >5000 )  and algo ==2):
         messagebox.showinfo("Input Size Recommendation","The number of blocks must be between 500 and 5000")
-
+    
+    if((BlockLength < n/100)  and algo == 5):
+        messagebox.showinfo("Input Size Recommendation","Make sure that M > n*0.01")
+    
+    if((BitStream_Length < 1000000)  and algo == 6):
+        messagebox.showinfo("Input Size Recommendation","Choose a minimum of 10^6 bits")
+        
     if(algo == 1):
         f = [frequencyTest(BitStream_Length,epsilon[i:i+BitStream_Length]) for i in range(0,BitStream_Length* Nb_of_BitStream,BitStream_Length)]
     if(algo == 2):
@@ -224,6 +251,11 @@ def Lancer():
         f = [P_value(BitStream_Length,epsilon[i:i+BitStream_Length]) for i in range(0,BitStream_Length* Nb_of_BitStream,BitStream_Length)] 
     if(algo == 4):
         f = [linearComplexityTest(BitStream_Length, BlockLength, epsilon[i:i+BitStream_Length]) for i in range(0,BitStream_Length* Nb_of_BitStream,BitStream_Length)] 
+    if(algo == 5):
+        f = [NonOverlappingTemplateMatching(BitStream_Length, BlockLength, Template,  epsilon[i:i+BitStream_Length]) for i in range(0,BitStream_Length* Nb_of_BitStream,BitStream_Length)] 
+    if(algo == 6):
+        f = [lOverlappingTemplateMatching(BitStream_Length, Template, epsilon[i:i+BitStream_Length]) for i in range(0,BitStream_Length* Nb_of_BitStream,BitStream_Length)] 
+        
     if(type_graphe == 1):
         hist(f)
     if(type_graphe == 2):
